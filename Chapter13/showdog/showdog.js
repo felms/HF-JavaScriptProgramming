ea79b27bd2af072865e94bc65792eb043adb920e
Felipe <felipemoreira1@gmail.com>
2020-05-06 20:53:32 -0300

    Chapter 13: Showdogs final test

--- Chapter13/showdog/showdog.js
+++ Chapter13/showdog/showdog.js
@@ -59,31 +59,17 @@ ShowDog.prototype.groom = function(){
 	console.log("Groom");
 }
 
-var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
-
-scotty.stack();
-scotty.bark();
-console.log(scotty.league);
-console.log(scotty.species);
-
-
 var fido = new Dog("Fido", "Mixed", 38);
-if(fido instanceof Dog){
-	console.log("Fido is a Dog");
-}
-
-if(fido instanceof ShowDog){
-	console.log("Fido is a ShowDog");
-}
+var fluffy = new Dog("Fluffy", "Poodle", 30);
+var spot = new Dog("Spot", "Chihuahua", 10);
 
 var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
-if(scotty instanceof Dog){
-	console.log("Scotty is a Dog");
-}
+var beatrice = new ShowDog("Beatrice", "Pomeranian", 5, "Hamilton");
 
-if(scotty instanceof ShowDog){
-	console.log("Scotty is a ShowDog");
-}
-
-console.log("Fido constructor is " + fido.constructor);
-console.log("Scotty constructor is " + scotty.constructor);
+fido.bark();
+fluffy.bark();
+spot.bark();
+scotty.bark();
+beatrice.bark();
+scotty.gait("Walk");
+beatrice.groom();
